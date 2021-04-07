24f63a767c9a60b41bfac77f4143063e3af84a3f
advait-t <60815910+advait-t@users.noreply.github.com>
2021-04-07 17:07:28 +0530

    Update bdt_app.py

--- bdt_app.py
+++ bdt_app.py
@@ -478,7 +478,8 @@ def fantasy_predictor(s14_data):
         try:
             bowl_test = bowl_preprocess_input_data(bowling['data'],bowling, Player, team_select1,toss_winner,chose_to,stadium_select)
             predbl = bowling['nn_model'].predict(bowl_test)
-            st.write('Predicted Fantasy Points:',str((predbl[0][0]).round(0)))
+            st.write('Predicted Fantasy Points:')
+            st.write((predbl[0][0]).round(0))
         except:
              try:
                  bat_test = bat_preprocess_input_data(batting['data'],batting, Player, team_select1,toss_winner,chose_to,stadium_select)
