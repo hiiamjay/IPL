08fc586198258e1f2f633caf37df9ca97582a6d0
hiiamjay <63848615+hiiamjay@users.noreply.github.com>
2021-04-07 18:50:04 +0530

    Update bdt_app.py

--- bdt_app.py
+++ bdt_app.py
@@ -442,8 +442,7 @@ def fantasy_predictor(s14_data):
         try:
             bat_test = bat_preprocess_input_data(batting['data'],batting, Player, team_select1,toss_winner,chose_to,stadium_select)
             predbt = batting['nn_model'].predict(bat_test)
-            output = 'Predicted Fantasy Points: '+str(predbt[0][0]).round(0)
-            st.header(output)
+            st.header('Predicted Fantasy Points: ')
             st.write((predbt[0][0]).round(0))
         except:
             try:
