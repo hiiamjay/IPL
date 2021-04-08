fb42278c2f0da51af0b4a156d925b4a934a0ef3e
hiiamjay <63848615+hiiamjay@users.noreply.github.com>
2021-04-08 14:38:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -487,6 +487,7 @@ def fantasy_predictor(s14_data):
 
         fantasy = pd.DataFrame(zip(list_player,list_bat,list_bowl,[list_bat[i] + list_bowl[i] for i in range(len(list_bat))]), columns=("Player","Batting Points","Bowling Points","Total Points"))
         fantasy = fantasy.sort_values(by=[sort_by], ascending=False)
+        fantasy = fantasy.reset_index(drop=True)
         st.table(fantasy)
     
     else:
