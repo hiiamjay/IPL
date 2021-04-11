c14586605ebb0365ffc3b682186914647849e0ef
advait-t <60815910+advait-t@users.noreply.github.com>
2021-04-11 15:45:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -601,3 +601,14 @@ elif option_select == 'Data Extractor':
     bdt_app(bat_data,bowl_data,player_data)
 else:
     fantasy_predictor(s14_data)
+
+import session_state  # Assuming SessionState.py lives on this folder
+
+session = session_state.get(run_id=0)
+
+slider_element = st.empty()
+
+if st.button("Reset"):
+  session.run_id += 1
+
+slider_element.slider("Slide me!", 0, 100, key=session.run_id)
